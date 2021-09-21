221f73f9fa57d1134ba2f68af04f54a0afa76c16
DialMyNumber <xcvxcvxv@naver.com>
2021-09-21 16:20:10 +0900

    Homework1

---
+++ Homework1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        System.out.print(" 정수를 입력하세요.\n");
+        int num = sc.nextInt();
+        sum=sum+num;
+        System.out.println(" 입력한 정수의 총 합: " + sum);
+        System.out.print(" 정수를 입력하세요.\n");
+        num = sc.nextInt();
+        sum=sum+num;
+        System.out.println(" 입력한 정수의 총 합: " + sum);
+        System.out.print(" 정수를 입력하세요.\n");
+        num = sc.nextInt();
+        sum=sum+num;
+        System.out.println(" 입력한 정수의 총 합: " + sum);
+        System.out.print(" 정수를 입력하세요.\n");
+        num = sc.nextInt();
+        sum=sum+num;
+        System.out.println(" 입력한 정수의 총 합: " + sum);
+        System.out.print(" 정수를 입력하세요.\n");
+        num = sc.nextInt();
+        sum=sum+num;
+        System.out.println(" 총 입력한 정수의 총 합: " + sum);
+    }
+}
